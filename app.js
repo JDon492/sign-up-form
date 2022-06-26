88f1a5652816c8234c4537425bd5d4dac22835c6
JDon492 <joshdonovan492@gmail.com>
2022-06-26 22:56:07 +0100

    Added red outline when password fields are incorrect

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ let confirmPass = document.querySelector("#confirm-pass");
 let form = document.querySelector("#form");
 let errorMessage = document.querySelector("#error-message");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
     if (password.value.length < 6) {
         e.preventDefault()
         errorMessage.innerText = ('Password must be atleast 6 characters long')
-    }
+        password.classList.add("errorFound")
+
+    } 
     if (password.value !== confirmPass.value ) {
         e.preventDefault()
         errorMessage.innerText = ('Passwords do not match')
+        password.classList.add("errorFound")
+        confirmPass.classList.add("errorFound")
     }
+})
+
+password.addEventListener("keyup", e => {
+    password.classList.remove("errorFound")
+    confirmPass.classList.remove("errorFound")
+    errorMessage.innerText = ("")
 })
