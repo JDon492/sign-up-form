58b1462b62f468172ac822dc5e5b8935f5f16370
JDon492 <joshdonovan492@gmail.com>
2022-06-26 17:58:26 +0100

    Added JS form validation

---
+++ app.js
@@ -0,0 +1,20 @@
+let firstName = document.querySelector("#first-name");
+let password = document.querySelector("#password-box");
+let confirmPass = document.querySelector("#confirm-pass");
+let form = document.querySelector("#form");
+let errorMessage = document.querySelector("#error-message");
+
+form.addEventListener("submit", (e) => {
+    if (password.value.length < 6) {
+        e.preventDefault()
+        errorMessage.innerText = ('Password must be atleast 6 characters long')
+    }
+    if (password.value !== confirmPass.value ) {
+        e.preventDefault()
+        errorMessage.innerText = ('Passwords do not match')
+    }
+    if (firstName.innertext !== /[A-Za-z]/) {
+        e.preventDefault()
+        errorMessage.innerText = ('Name must not contain numbers')
+    }
+})
